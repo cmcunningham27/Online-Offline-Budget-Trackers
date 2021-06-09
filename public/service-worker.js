90a0b3671c5b5445b3e0cb351dd34f3559e2dfac
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-09 11:36:01 -0700

    declared 2 variables: one for pre-cache and one for when the app is offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,12 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/index.js',
     '/style.css',
+    '/db.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
 ];
+
+const CACHE_NAME = 'static-cache-v1';
+const RUNTIME_CACHE = 'runtime-cache';
 
