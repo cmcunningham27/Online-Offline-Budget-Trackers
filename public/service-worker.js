96d1e7a5f88ad20b691c67ee4a23e0da73613e65
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-09 11:28:43 -0700

    created service-worker.js file, declared and variable with value for items that need to be cached for offline use

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/style.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+];
+
