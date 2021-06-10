2bfc9688410df5ba1d3a096476d4b8bc68d12360
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-09 21:56:12 -0700

    Code added for when version is updated from the old version to the new version

--- public/db.js
+++ public/db.js
@@ -3,7 +3,14 @@ let db;
 //Opens indexedDB
 const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = event => {
+    const { oldVersion } = event;
+    const newVersion = event.newVersion || db.version;
+
+    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+}
+
 //run this code if indexedDB opens successfully
 request.onsuccess = ({target}) => {
     db = target.result;
-}
+};
