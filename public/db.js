857653b94965b9c2f8c2218635c9b32d01d4c516
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-09 21:59:21 -0700

    when upgraded db gets a value and if the db does not have an Object Store yet then an Object Store will be created

--- public/db.js
+++ public/db.js
@@ -8,7 +8,15 @@ request.onupgradeneeded = event => {
     const newVersion = event.newVersion || db.version;
 
     console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-}
+
+    db - event.target.result;
+
+    if(db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', {
+            autoincrement: true
+        });
+    }
+};
 
 //run this code if indexedDB opens successfully
 request.onsuccess = ({target}) => {
