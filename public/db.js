9259ffaa6e8db3badbaae390a8d2761986bcf727
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-09 21:52:51 -0700

    added code for when indexedDB opens succesfully

--- public/db.js
+++ public/db.js
@@ -1,3 +1,9 @@
+//declares db to be changed later
 let db;
+//Opens indexedDB
 const request = indexedDB.open("budget", 1);
 
+//run this code if indexedDB opens successfully
+request.onsuccess = ({target}) => {
+    db = target.result;
+}
